8e6ffeccbf7301678d1c0bdb5cdf642b84736657
analimateixeira-hash <ana.lima.teixeira@escola.pr.gov.br>
2025-08-25 10:49:49 -0300

    crie as listas

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-convidados_da_festa = ['Ana', 'Diego']
+#Lista de convidados para a festa 
+convidados_da_festa = ['Ana', 'Diego', 'yuliany']
+
+status_presenca = {}
+
+lista_de_chegadas = ['Ana','Diego'] 
