34da321308a3b79d278d76beb6ed891ceb89e25d
analimateixeira-hash <ana.lima.teixeira@escola.pr.gov.br>
2025-08-27 08:46:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,21 @@ convidados_da_festa = ['Ana', 'Diego', 'yuliany']
 status_presenca = {}
 
 lista_de_chegadas = ['Ana','Diego'] 
+convidados_da_festa = ['Maria', 'João','Ana', 'Carlos', 'Mariana']
+
+status_presenca = {}
+
+print('--- Verificação da lista de convidados ---')
+
+lista_de_chegada = ['João', 'Ana', 'Pedro', 'Maria'] 
+
+for pessoa in lista_de_chegada:
+    if pessoa in convidados_da_festa:
+        print(f'Olá, {pessoa}! Bem-vindo(a) à festa.')
+        status_presenca[pessoa] = 'Confirmado'
+
+    else:
+        print(f'Desculpe, {pessoa}. Seu nome não está na lista')
+        status_presenca[pessoa] = 'Não Convidado'
+
+
